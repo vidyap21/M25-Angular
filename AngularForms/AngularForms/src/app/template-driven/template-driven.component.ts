3da9012638704134e582e7ab8b7e67185921a5cd
VidyaPaygude <70515158+vidyap21@users.noreply.github.com>
2022-12-20 20:19:22 +0530

    Add files via upload

---
+++ AngularForms/AngularForms/src/app/template-driven/template-driven.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-template-driven',
+  templateUrl: './template-driven.component.html',
+  styleUrls: ['./template-driven.component.css']
+})
+export class TemplateDrivenComponent {
+  onSubmit(form: NgForm): void{
+    console.log(form.value);
+  }
+}
